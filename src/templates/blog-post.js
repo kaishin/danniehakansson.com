ea95bf4255dd97fbd3d73cce4d0b99aae2a36bf9
Dannie Håkansson <danniehakan@gmail.com>
2020-09-26 10:58:09 +0200

    Fixed resolve issue

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BlogPostTemplate = ({ data }) => {
